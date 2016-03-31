7ba512afb542256bfb104657f2d3a176a18d46df
Gene Servadei <gene@sphericalstudios.com.au>
2016-03-31 14:59:59 +1100

    Initial commit

---
+++ SimpleMap/SatelliteMapViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SatelliteMapViewController.swift
+//  Simple Map
+//
+//  Created by Gene Servadei on 21/01/2016.
+//  Copyright © 2016 Spherical Studios. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class SatelliteMapViewController: UIViewController {
+
+    @IBOutlet weak var satelliteMKV: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
---
+++ SimpleMap/StandardMapViewController.swift
@@ -0,0 +1,27 @@
+//
+//  StandardMapViewController.swift
+//  Simple Map
+//
+//  Created by Gene Servadei on 21/01/2016.
+//  Copyright © 2016 Spherical Studios. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class StandardMapViewController: UIViewController {
+
+    @IBOutlet weak var standardMKV: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
